0c3dcb68c4140183b589fced5fb1088bba2985e4
raspmldlgycchag <raspmldlgycchang8753@naver.com>
2022-09-04 21:15:52 +0900

    Ref #1, 트로미노 최종 업데이트

--- DivideandConquerExample/트로미노.py
+++ DivideandConquerExample/트로미노.py
@@ -2,25 +2,24 @@ def tromino(size, board, srow, scol,xrow, xcol):
 
     # 구멍이 없는 게 확인되면 fill 함수로 구멍을 넣어주기
     # 구멍 위치는 mrow-1, mcol-1
-    mrow = srow + (size // 2)
-    mcol = scol + (size // 2)
 
     where = whereis(size, srow, scol,xrow, xcol)
     if size==2:
         fill(size, board, srow, scol, where)
     else:
-        fill(size, board,srow+mrow-1, scol+mcol-1, where)
-        # 구멍 위치를 찾은 후에
+        m = size // 2
+        fill(size, board,srow+m-1, scol+m-1, where)
+        # 구멍의 사분면에서의 위치를 찾은 후에
         hrow, hcol = hole(1, size, where, board, srow, scol, xrow, xcol)
         # 현재 사사분면으로 나눠진 상태를 모든 사분면에 대해서 호출해야 하니까
         # 현재 srow, scol, size 를 써야 한다
-        tromino(size//2, board, srow, scol, xrow, xcol)
+        tromino(size//2, board, srow, scol, hrow, hcol)
         hrow, hcol = hole(2, size, where, board, srow, scol, xrow, xcol)
-        tromino(size//2, board, srow, mcol,xrow, xcol)
+        tromino(size//2, board, srow, scol+m,hrow, hcol)
         hrow, hcol = hole(3, size, where, board, srow, scol, xrow, xcol)
-        tromino(size//2, board, mrow, scol, xrow, xcol)
+        tromino(size//2, board, srow+m, scol, hrow, hcol)
         hrow, hcol = hole(4, size, where, board, srow, scol, xrow, xcol)
-        tromino(size//2, board, mrow, mcol, xrow, xcol)
+        tromino(size//2, board, srow+m, scol+m, hrow, hcol)
 
 def fill(n, board, srow, scol, where):
     if where!=1:    board[srow][scol] = n
@@ -45,3 +44,15 @@ def whereis(size, srow, scol, xrow, xcol):
         return 1 if xcol-scol<m else 2
     else:
         return 3 if xcol-scol<m else 4
+
+def print_board(board):
+    for i in board:
+        for j in i:
+            print(j, end = " ")
+        print(sep='\n')
+
+n, xrow, xcol = map(int, input().split())
+board  = [[0 for _ in range(n)] for _ in range(n)]
+srow, scol = 0, 0
+tromino(n, board, srow, scol, xrow, xcol)
+print_board(board)
